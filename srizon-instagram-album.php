420bb0e3d2b02f62d1fcfb385d699c91723dae1e
Afzal Hossain <afzal.csedu@gmail.com>
2017-07-06 16:38:06 +0600

    init

---
+++ srizon-instagram-album.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Srizon Instagram Album
+Plugin URI: https://www.srizon.com/srizon-instagram-album
+Description: This Plugin is designed to show your instagram photos into your WordPress site. You can also show public photos from other instagram users.
+Text Domain: srizon-instagram-album
+Domain Path: /languages
+Version: 1.0
+Author: Afzal
+Author URI: https://www.srizon.com/contact
+*/
