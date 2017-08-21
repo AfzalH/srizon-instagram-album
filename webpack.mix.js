549fbbacc51c2e0c0fbe8b6fd1177e19d8f1dc80
Afzal Hossain <afzal.csedu@gmail.com>
2017-08-21 16:49:48 +0600

    react and react-dom local copy added

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,12 +11,12 @@ let mix = require('laravel-mix');
  |
  */
 
-// mix.webpackConfig({
-//     externals: {
-//         "react": "React",
-//         "react-dom": "ReactDOM"
-//     }
-// });
+mix.webpackConfig({
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+});
 
 mix.react('assets/admin/js/app.jsx', 'admin/resources')
     .react('assets/site/js/app.jsx', 'site/resources')
@@ -24,5 +24,9 @@ mix.react('assets/admin/js/app.jsx', 'admin/resources')
     .sass('assets/site/sass/app.scss', 'site/resources')
     .sass('assets/lib/srizon-materialize/sass/materialize.scss', 'admin/resources')
     .sass('assets/lib/srizon-materialize-site/sass/materialize.scss', 'site/resources')
+    .copy('assets/lib/react/react.min.js', 'admin/resources/react.min.js')
+    .copy('assets/lib/react/react-dom.min.js', 'admin/resources/react-dom.min.js')
+    .copy('assets/lib/react/react.min.js', 'site/resources/react.min.js')
+    .copy('assets/lib/react/react-dom.min.js', 'site/resources/react-dom.min.js')
     .copy('assets/lib/srizon-materialize/js/bin/materialize.min.js', 'admin/resources/materialize.js')
     .copy('assets/lib/srizon-materialize/js/bin/materialize.min.js', 'site/resources/materialize.js');
