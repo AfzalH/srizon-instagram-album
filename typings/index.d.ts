e49eacb67c93c939b96fd5f561f917215227aafe
Afzal Hossain <afzal.csedu@gmail.com>
2017-08-21 13:36:52 +0600

    added typings

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/axios/index.d.ts" />
+/// <reference path="globals/react-measure/index.d.ts" />
+/// <reference path="globals/redux-thunk/index.d.ts" />
+/// <reference path="globals/redux/index.d.ts" />
